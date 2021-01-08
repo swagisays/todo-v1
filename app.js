92beddae05b879a67f54d3a4fb5262200be3f09c
Shubham Swami <swamishubham614@gmail.com>
2021-01-08 19:18:50 +0530

    adding default elements to listdb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');// require express
 const bodyParser = require('body-parser');// require body parser
+const mongoose = require('mongoose');//require mongoose for db
 
 const app = express();// initialize express to app
 app.use(bodyParser.urlencoded({//syntext to use body parser
@@ -7,32 +8,58 @@ app.use(bodyParser.urlencoded({//syntext to use body parser
 }));
 app.use(express.static("public"));// sending public files to user
 app.set('view engine', 'ejs');// setting up ejs module
-var items = [];// arrey to store todo list items
+mongoose.connect("mongodb://localhost:27017/tododb");
 
+const ItemScema = {
+  name:String
+};
+const Item = mongoose.model("item",ItemScema);
+
+const item1 = new Item({
+  name:"chai pee"
+});
+const item2 = new Item({
+  name:"kup dho lo"
+});
+const item3 = new Item({
+  name:"shinke mai rakh do"
+});
+
+const defaultItems = [item1,item2,item3];
 
 app.get("/", function(req,res) {// geting req to rout route
-  const day = new Date();// useing jave to display local date
-  const options = {//seting up date schema
-    weekday: "long",//weekday as full String
-    day: "numeric",// day in numeric digit
-    month: "long",// month in full string
-  }
-  const today = day.toLocaleDateString("en-US", options);//store date in today const
 
-  res.render("list", {//sending data to ejs list template
-    kindOfDay: today,//storing date in kindaofday ejs variable
-    item: items//storing item arrey in ejs items variable
-  });
+  Item.find(function (err,foundItems) {
+    if (foundItems.length===0) {
+      Item.insertMany(defaultItems,function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("success");
+        }
+      });
+    };
+    res.redirect("/");
 
+    else{
+      res.render("list", {//sending data to ejs list template
+        kindOfDay: "today",//storing date in kindaofday ejs variable
+        item: foundItems//storing item arrey in ejs items variable
+      });
+    };
+
+  })
 })
+
+
 
 app.post("/", function(req, res) {// geting list items from user
   let item = req.body.newItem;//storing list newitem to item
   items.push(item);//pushing new item into items array
   res.redirect("/");//redirecting to get request
 
-})
+});
 
 app.listen(1705, function() {//listioning on port 1705
   console.log("server is running on port 1705");//sending msg of conformation
-})
+});
