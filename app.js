e42474c99b6294540b75bec2a3a105db83d91de9
Shubham Swami <swamishubham614@gmail.com>
2021-01-27 09:21:18 +0530

    error login

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express'); // requiring express
 const bodyParser = require('body-parser'); // requiring body parser
 const mongoose = require('mongoose'); //requiring mongoose for db
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express(); // initialize express to app
 app.use(bodyParser.urlencoded({ //syntext to use body parser
@@ -10,17 +14,40 @@ app.use(bodyParser.urlencoded({ //syntext to use body parser
 app.use(express.static("public")); // sending public files to user
 app.set('view engine', 'ejs'); // setting up ejs module
 
-mongoose.connect(process.env.MDB_CONNECT+"/tododb", {// createing tododb data base & running mongodb server on local host
-  useUnifiedTopology: true,// removing depication WARNING
-  useFindAndModify: false,// removing depication WARNING
-  useNewUrlParser: true// removing depication WARNING
-});
-
-// mongoose.connect("mongodb://localhost:27017/tododb", {// createing tododb data base & running mongodb server on local host
+app.use(session({
+  secret: process.env.SECRET,
+  resave:false,
+  saveUninitialized:true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// mongoose.connect(process.env.MDB_CONNECT+"/tododb", {// createing tododb data base & running mongodb server on local host
 //   useUnifiedTopology: true,// removing depication WARNING
 //   useFindAndModify: false,// removing depication WARNING
 //   useNewUrlParser: true// removing depication WARNING
 // });
+
+mongoose.connect("mongodb://localhost:27017/tododb", {// createing tododb data base & running mongodb server on local host
+  useUnifiedTopology: true,// removing depication WARNING
+  useFindAndModify: false,// removing depication WARNING
+  useNewUrlParser: true,// removing depication WARNING
+  useCreateIndex: true
+});
+
+
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String   
+});
+UserSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("user",UserSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const ItemSchema = {// createing schema for items
   value: String// only contain a string element
@@ -33,59 +60,88 @@ const ListSchema = {//creating schema for new list
 }
 const List = mongoose.model("list", ListSchema);// creating new colection called lists
 
+
 app.get("/", function(req, res) { // geting req to home root
+  console.log(req.isAuthenticated);
+  if (!req.isAuthenticated) {
+    Item.find({}, function(err, foundItems) {// finding elements in list
+      if (foundItems.length === 0) {// if their's no element
+        res.render("list", {// send data to list
+          listTitle: "Today",// sending list title
+          noItem: true//making no item var to true
+        });
+      } else {// if their have some elements present
+        res.render("list", { //sending data to ejs list template
+          listTitle: "Today", // sending list title
+          noItem: false,// their have elements so sending false
+          item: foundItems //storing item arrey in ejs items variable
+         });
+      }//else end
+    });//find item end
+  }else{
+    res.redirect("/register");
+  }
 
-  Item.find({}, function(err, foundItems) {// finding elements in list
-    if (foundItems.length === 0) {// if their's no element
-      res.render("list", {// send data to list
-        listTitle: "Today",// sending list title
-        noItem: true//making no item var to true
-      });
-    } else {// if their have some elements present
-      res.render("list", { //sending data to ejs list template
-        listTitle: "Today", // sending list title
-        noItem: false,// their have elements so sending false
-        item: foundItems //storing item arrey in ejs items variable
-       });
-    }//else end
-  });//find item end
+  
 });// get reqest end
+app.get("/register",function (req,res) {
+  res.render("reg");
+  
+});
 
-app.get("/:custumList", function(req, res) {// geting custom list made by user
-  let custumListUser = req.params.custumList;// making get req to custom list
-  let custumListName = custumListUser[0].toUpperCase() + custumListUser.slice(1).toLowerCase();//changing custom list first letter to upper case and remaning lowecase
 
-  List.findOne({name: custumListName}, function(err, foundList) {// finding if list is exist or not
-    if (!err) {// if their is no error
-      if (!foundList) {// if list is not exist create new list
-        list = new List({// creating new list
-          name: custumListName,// applying custom name to list
-          items: []//storing a empty arry coz their's non items yet
-        });// new list close
-        list.save();// save new list to data base
-        res.redirect("/" + custumListName);// redirecting to new list page
-      } else {  // if list is already exist then showing the list
-        if (foundList.items.length == 0) {// if their's no items in list
-          res.render("list", {// sending data to list or list.ejs
-            listTitle: foundList.name,// sending list  title to list.ejs
-            noItem: true// no items so send true in list.ejs
-          });// sending data compleate
-        } else {// if their has items in list so send them to list.ejs
-            res.render("list", { //sending data to ejs list template
-            listTitle: foundList.name, //storing list name in ejs listTitle variable
-            noItem: false,// their has items so no items is false send to list.ejs
-            item: foundList.items //storing item arrey in ejs items variable
-          });
-        }// child else statement end
-      }// parent else statement end
-    }// whole if element end which say's no error
-    else{// if their has error
-      console.log(err);// log(show) error to console
-    }//error else end
+// app.get("/:custumList", function(req, res) {// geting custom list made by user
+//   let custumListUser = req.params.custumList;// making get req to custom list
+//   let custumListName = custumListUser[0].toUpperCase() + custumListUser.slice(1).toLowerCase();//changing custom list first letter to upper case and remaning lowecase
 
-  });// find element's in list end
+//   List.findOne({name: custumListName}, function(err, foundList) {// finding if list is exist or not
+//     if (!err) {// if their is no error
+//       if (!foundList) {// if list is not exist create new list
+//         list = new List({// creating new list
+//           name: custumListName,// applying custom name to list
+//           items: []//storing a empty arry coz their's non items yet
+//         });// new list close
+//         list.save();// save new list to data base
+//         res.redirect("/" + custumListName);// redirecting to new list page
+//       } else {  // if list is already exist then showing the list
+//         if (foundList.items.length == 0) {// if their's no items in list
+//           res.render("list", {// sending data to list or list.ejs
+//             listTitle: foundList.name,// sending list  title to list.ejs
+//             noItem: true// no items so send true in list.ejs
+//           });// sending data compleate
+//         } else {// if their has items in list so send them to list.ejs
+//             res.render("list", { //sending data to ejs list template
+//             listTitle: foundList.name, //storing list name in ejs listTitle variable
+//             noItem: false,// their has items so no items is false send to list.ejs
+//             item: foundList.items //storing item arrey in ejs items variable
+//           });
+//         }// child else statement end
+//       }// parent else statement end
+//     }// whole if element end which say's no error
+//     else{// if their has error
+//       console.log(err);// log(show) error to console
+//     }//error else end
 
-});// custom list get route end
+//   });// find element's in list end
+
+// });// custom list get route end
+
+
+app.post("/register",function (req,res) {
+  console.log(req.body.email);  
+  User.register({username: req.body.email}, req.body.password , function (err,user) {
+    if (err) {
+      console.log(err);      
+      res.render("reg");
+    } else {console.log("sjdfh");
+      passport.authenticate("local"),(function (req,res,next) {
+      console.log("1");
+      res.redirect("secrets");
+      console.log("2");
+    });
+    }
+  });
+});
 
 app.post("/", function(req, res) { // geting list items from user
   let itemValue = req.body.newItem; //storing list item to itemValue
@@ -135,11 +191,11 @@ app.post("/delete", function(req, res) {// post req if user want to deleate an i
   }// end of else statement
 });//deleteing elements post req end
 
-let port = process.env.PORT;
-if (port==null || port=="") {
-  port=1705;
-}
+// let port = process.env.PORT;
+// if (port==null || port=="") {
+//   port=1705;
+// }
 
-app.listen(port || 1705, function() { //listioning on port 1705
+app.listen(3000, function() { //listioning on port 1705
   console.log("server is running on port 1705"); //sending msg of conformation
 });
